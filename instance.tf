0986d307c43d4c48965e750beaf41fde826229c5
janakthakkar <82100311+janakthakkar@users.noreply.github.com>
2021-12-28 13:30:29 +0530

    removed security group

--- instance.tf
+++ instance.tf
@@ -10,14 +10,13 @@ resource "aws_instance" "net4io" {
     ami = "ami-0a19ee9fe8c518fbf"
     instance_type = "t2.micro"
     key_name = "net4io"
-    security_groups = [ "dynamic-sg" ]
 
     tags = {
       Name = "${var.instance_name}"
     }
 }
 
-output "instance_public_ip" {
-    description = "AWS Instance public/elastic IP"
-    value = aws_instance.net4io.public_ip
+output "instance_private_ip" {
+    description = "AWS Instance private IP"
+    value = aws_instance.net4io.private_ip
 }
