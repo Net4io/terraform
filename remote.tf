ac936b330357e8f8c693c8f9594774707ce9c604
janakthakkar <janak@imorphr.com>
2022-01-24 17:06:26 +0530

    remote state

--- remote.tf
+++ remote.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "net4io-terraform-state"
-    key    = "terraform.tfstate"
+    key    = "instance/terraform.tfstate"
     region = "eu-west-2"
     dynamodb_table = "terraform"
   }
